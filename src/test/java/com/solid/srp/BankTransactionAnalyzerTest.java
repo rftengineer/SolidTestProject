2b1327786330568b3d4d6a18616b78237d3c193d
Steve Shim <nightpw1@gmail.com>
2021-04-29 00:32:12 +0900

    feat: Java7 nio Paths이용, 경로에 있는 파일 정상 조회 확인

--- src/test/java/com/solid/srp/BankTransactionAnalyzerTest.java
+++ src/test/java/com/solid/srp/BankTransactionAnalyzerTest.java
@@ -1,4 +1,19 @@
 package com.solid.srp;
 
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BankTransactionAnalyzerTest {
+
+    @Test
+    void readFile_from_resources() {
+        Path path = Paths.get("src/java/resources/DataSample.csv");
+        String answer = "DataSample.csv";
+
+        assertThat(path.getFileName().toString()).isEqualTo(answer);
+    }
 }
