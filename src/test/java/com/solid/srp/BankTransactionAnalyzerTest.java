4e7dab83290964bd76010aafc8947b9ca0cb65ab
Steve Shim <nightpw1@gmail.com>
2021-04-29 00:55:08 +0900

    feat: 경로확인 및 파일 로드, 존재여부 확인 Testcode 작성

--- src/test/java/com/solid/srp/BankTransactionAnalyzerTest.java
+++ src/test/java/com/solid/srp/BankTransactionAnalyzerTest.java
@@ -2,8 +2,11 @@ package com.solid.srp;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,9 +14,18 @@ public class BankTransactionAnalyzerTest {
 
     @Test
     void readFile_from_resources() {
-        Path path = Paths.get("src/java/resources/DataSample.csv");
+        Path path = Paths.get("/src/main/resources/DataSample.csv");
         String answer = "DataSample.csv";
 
         assertThat(path.getFileName().toString()).isEqualTo(answer);
+    }
+
+    @Test
+    void readLine_and_seperate() throws IOException {
+        Path path = Paths.get("src/main/resources/DataSample.csv");
+        List<String> data = Files.readAllLines(path);
+        // Check File is exists
+        assertThat(Files.exists(path)).isTrue();
+
     }
 }
